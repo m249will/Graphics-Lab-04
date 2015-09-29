07555980b813f5a9c3efbaa2ca10eb03312e2cd4
Will Schiesswohl <william.schiesswohl@vtsd.com>
2015-09-29 08:32:35 -0400

    Update GraphicsLab04.java

--- src/GraphicsLab04.java
+++ src/GraphicsLab04.java
@@ -4,7 +4,7 @@
  * AP Computer Science Period 2
  */
 
-/* g.drawLine
+/** g.drawLine
  * g.drawRect
  * g.drawOval(x,y,width,height)
  * g.drawArc(x,y,width,height,start,end)
@@ -34,25 +34,41 @@ public class GraphicsLab04 extends Applet
         g.drawArc(65,95,240,180,0,360);
         g.drawArc(65,125,240,120,0,360);
         g.drawArc(65,155,240,60,0,360);
-        
-        
-        
         //////////////Draw Inscribed/circumscribed triangle/////////////
-        
-        
-        
-        
+        //large circle
+        g.drawOval(600,500,200,200);
+        //triangle
+        g.drawLine(600,600,777,538);
+        g.drawLine(700,700,777,538);
+        g.drawLine(600,600,700,700);
+        //innermost circle
+        g.drawOval(640,574,93,93);
         ////////////////////Draw APCS/////////////////////
-        
-        
-        
-        
-        
+        //this is for the 'A'
+        g.fillRect(25,500,20,100);
+        g.fillRect(25,500,60,20);
+        g.fillRect(25,540,60,20);
+        g.fillRect(65,500,20,100);
+        //this is for the 'P'
+        g.fillRect(105,500,20,100);
+        g.fillRect(105,500,60,20);
+        g.fillRect(105,540,60,20);
+        g.fillRect(145,500,20,60);
+        //this is for the 'C'
+        g.fillRect(185,500,60,20);
+        g.fillRect(185,500,20,100);
+        g.fillRect(185,580,60,20);
+        //this is for the 'S'
+        g.fillRect(265,500,60,20);
+        g.fillRect(265,500,20,60);
+        g.fillRect(265,540,60,20);
+        g.fillRect(305,540,20,60);
+        g.fillRect(265,580,60,20);        
         ///////////////////Draw pacmen Flower////////////////////
-        g.fillArc(650,250,125,125,45,270);
-        g.fillArc(570,170,125,125,135,270);
-        g.fillArc(570,330,125,125,315,270);
-        g.fillArc(490,250,125,125,225,270);
+        g.fillArc(650,250,125,125,45,270);//right pacman
+        g.fillArc(570,170,125,125,135,270);//top pacman
+        g.fillArc(570,330,125,125,315,270);//bottom pacman
+        g.fillArc(490,250,125,125,225,270);//left pacman
         
     }
 }
